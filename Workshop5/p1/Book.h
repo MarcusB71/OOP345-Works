5c3d7bf5ab983a967d76120b16de6f239bec697e
Marcus Brown <marcus.brown1234567@gmail.com>
2023-10-31 20:34:06 -0400

    finished p1

---
+++ Workshop5/p1/Book.h
@@ -0,0 +1,30 @@
+#ifndef SDDS_BOOK_H
+#define SDDS_BOOK_H
+#include <iostream>
+#include <string>
+namespace sdds {
+    class Book
+    {
+    private:
+        std::string m_author{};
+        std::string m_title{};
+        std::string m_COFP{}; // country of publciation
+        size_t m_year{};
+        double m_price{};
+        std::string m_desc{};
+    public:
+        Book();
+        Book(const std::string& strBook);
+        ~Book();
+        Book(const Book& other);
+        Book& operator=(const Book& other);
+        const std::string& title() const;
+        const std::string& country() const;
+        const size_t& year() const;
+        double& price();
+        static std::string trim(const std::string& str);
+        std::ostream& print(std::ostream& os)const;
+    };
+    std::ostream& operator<<(std::ostream& os, const Book& b);
+}
+#endif
